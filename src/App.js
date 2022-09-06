946e25a0cdb786c72e02dd72105152677cf87dd9
J-ATJ <jyoruge6798@gmail.com>
2022-09-05 21:13:01 -0500

    Added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ReactPlayer from 'react-player';
+import { useState } from 'react';
+
 function App() {
+  const [url, setUrl] = useState('https://www.youtube.com/watch?v=0Ml_lqhkysM');
+
+  const obtenerURL = eventoInput => {   //función que permite obtener la url del input y cambia la const url por lo que el video en pantalla cambia.
+    if (eventoInput.target.value.trim()) {
+      setUrl(eventoInput.target.value);
+    }
+    else {
+      setUrl('https://www.youtube.com/watch?v=0Ml_lqhkysM');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img className='logo' src={logo} />
+        <h1>Video Player</h1>
+        <img className='logo' src={logo} />
       </header>
+      <div className='bloc'>
+        <h3>Just paste the link of the video or live stream you want to play!</h3>
+        <div className='videoPlayer'>
+          <ReactPlayer
+            className='video'
+            url={url}
+            playing={false}
+            controls={true}
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1 }
+              },
+              facebook: {
+                appId: '12345'
+              }
+            }}
+          />
+        </div>
+        <h3>Supported YouTube, Facebook and Twitch</h3>
+      </div>
+
+      <input type="text" placeholder=' URL' className='input' onChange={obtenerURL}></input>
     </div>
   );
 }
